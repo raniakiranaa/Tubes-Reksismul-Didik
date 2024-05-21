a7b3c9c0a1b67b14a4cfc97a0085e1f70e8f9234
karinarahadiani <18221104@std.stei.itb.ac.id>
2024-05-21 14:44:58 +0700

    home

---
+++ src/app/home/page.js
@@ -0,0 +1,18 @@
+import { CardStack } from "@/components/shares/card"
+import tv from '/public/images/home/tv.png'
+import modul from '/public/images/home/modul.png'
+
+export default function app() {
+    return (
+        <div className="flex min-h-screen flex-col p-24">
+            <div className="flex flex-col items-start">
+                <div className="text-main-text text-left semibold-64">🙌🏻 Welcome Back!</div>
+                <p className="text-sub-text regular-18 pt-4 text-justify">Ready to dive into today's session? Let’s explore Bahasa Indonesia together and unlock new possibilities! ⭐️</p>
+            </div>
+            <div className="flex flex-row pt-16 justify-center space-x-32">
+                    <CardStack linkPage="/talkmenu" image={modul} description="Interactive learning with speech recognition & peer exchange."/>
+                    <CardStack linkPage="/speechmenu" image={tv} description="Real-time practice with other user speakers via video conferencing."/>
+            </div>
+        </div>
+    )
+}
